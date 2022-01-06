23e5317eec94442a6bd805c3f7d220c528087555
shima11 <shima.jin@icloud.com>
2022-01-06 18:02:22 +0900

    Initial Commit

---
+++ window-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  window-practice
+//
+//  Created by Jinsei Shima on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
